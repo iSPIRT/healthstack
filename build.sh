b3f5c9eb298f255d311b4a4dfa54a4a061254098
Ranjith Ramachandra <ranjith19@gmail.com>
2020-08-26 18:47:15 +0530

    add build sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+rm -rf build
+mkdir build
+mkdir build/spec
+find src -name \*.html -exec cp {} build \;
+find src -name \*.md -exec cp {} build \;
+
+for filename in src/spec/*.yaml; do
+    basename=$(basename "$filename")
+    filenamewithoutext="${basename%.*}.json"
+    echo $basename $filenamewithoutext
+    node_modules/yamljs/bin/yaml2json $filename > build/spec/$filenamewithoutext
+done
+
+cd build && python -m SimpleHTTPServer 4321
---
+++ src/README.md
@@ -0,0 +1,13 @@
+# healthstack
+
+<ul>
+    <li>
+        <a href="./consent-manager.html">Consent Manager</a>
+    </li>
+    <li>
+        <a href="./gateway.html">Gateway</a>
+    </li>
+    <li>
+        <a href="./health-repository.html">Health Repository</a>
+    </li>
+</ul>
