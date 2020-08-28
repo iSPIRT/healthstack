efd46cc072fe878fcecfb23f82b9871810a5bf57
Ranjith Ramachandra <ranjith19@gmail.com>
2020-08-28 10:11:47 +0530

    add x-logo

--- make_api_docs.py
+++ make_api_docs.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 
 api_spec_file_name = sys.argv[1]
 api_spec_file_name_without_ext = api_spec_file_name.replace('.json', '')
@@ -11,3 +12,13 @@ content = open(template_file_path).read().replace('SPEC_FILE', api_spec_file_nam
 
 build_path = os.path.join(cwd, 'build', api_spec_file_name_without_ext + '.html')
 open(build_path, 'w').write(content)
+
+json_file_path = os.path.join(cwd, 'build/spec', api_spec_file_name)
+content = open(json_file_path).read()
+
+spec = json.loads(content)
+spec['info']['x-logo'] = {
+      "url": "./media/ispirt-logo.svg",
+      "href": "/"
+    }
+open(json_file_path, 'w').write(json.dumps(spec, indent=2))
